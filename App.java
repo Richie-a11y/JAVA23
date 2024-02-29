7e6a1e8251de9eabddf3aed72701a1513b21cf83
Richie-a11y <ochiengrich313@gmai.com>
2024-02-29 18:36:57 +0300

    Added a file

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static int add(int a,int b){
+    return a +b;
+    }
+    
+    public static void main(String[] args) {
+        App a = new App();
+        
+        System.err.print(App.add(49, 3));
+   
+    }
+    
+}
